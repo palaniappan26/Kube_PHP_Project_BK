7d658308e53cac89833068094504f7f719f9b692
palaniappan26 <106897831+palaniappan26@users.noreply.github.com>
2022-09-13 21:30:58 +0800

    Update config.php

--- website/src/config.php
+++ website/src/config.php
@@ -8,7 +8,7 @@
 function config($key = '')
 {
     $config = [
-        'name' => 'Simple PHP Website--tested Using Kubernetive palani',
+        'name' => 'Simple PHP Website--tested Using Kubernetive',
         'nav_menu' => [
             '' => 'Home',
             'content/about-us.php' => 'About Us',
